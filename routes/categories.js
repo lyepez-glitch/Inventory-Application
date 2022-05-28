c57903bae612fb527311d83d2d6e724d5c62e02f
Lucas Yepez <lucasyepez99@gmail.com>
2022-05-28 01:03:37 -0500

    can add items to category

--- routes/categories.js
+++ routes/categories.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router()
 const Category = require('../models/categorySchema');
+const mongoose = require('mongoose')
 const Item = require('../models/itemsSchema');
 router.get('/', async(req, res) => {
     const found_categories = await Category.find({}).populate('items')
@@ -12,19 +13,53 @@ router.get('/create', async(req, res) => {
 })
 
 router.post('/create', async(req, res) => {
-    const { category, description } = req.body;
-    const new_category = new Category({ name: category, description })
+    let { category, description, items } = req.body;
+    items = items.split(',')
+    var itemsCopy = items;
+    const new_category = new Category({ name: category, description, items: [] })
+
     await new_category.save()
+
+    console.log(new_category)
+
     console.log('Category Created')
     res.redirect('/categories')
+
+})
+
+router.get('/:id/addItems', async(req, res) => {
+    let cat_name = await Category.findById(req.params.id);
+    const { name } = cat_name;
+    res.render('add_items', { cat_name: name })
+})
+
+router.post('/:id/addItems', async(req, res) => {
+    let { cat, items } = req.body;
+    let found_cat = await Category.findById(req.params.id)
+    items = items.split(',');
+    items.forEach(async(item) => {
+        console.log('item', item)
+        let new_item = new Item({
+            name: item
+        });
+        found_cat.items.push(new_item)
+        let saved_item = await new_item.save();
+
+
+
+    })
+    found_cat = await found_cat.save()
+    console.log('fcat', found_cat)
+    res.redirect('/categories')
+
 
 })
 
 router.get('/:id', async(req, res) => {
     const { id } = req.params;
     Category.findById(id).populate('items').then(cat => {
-        console.log('test', cat)
-        res.render('category_detail', { cat, items: cat.items })
+        console.log('test', cat, cat.items)
+        res.render('category_detail', { cat, items: cat.items, itemURL: `${req.params.id}/AddItems` })
     })
 })
 
--- routes/items.js
+++ routes/items.js
@@ -7,13 +7,15 @@ router.get('/', async(req, res) => {
     res.render('items', { found_items })
 })
 
+
 router.get('/:id', async(req, res) => {
     const { id } = req.params;
     const found_item = await Item.findById(id)
 
     res.render('items_detail', { item: found_item })
-
 })
+
+
 
 router.post(':')
 module.exports = router
